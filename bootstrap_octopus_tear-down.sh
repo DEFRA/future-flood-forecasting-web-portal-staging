ecee57b078094466504ba6b6151f3c0d59aff7a4
JackSheppardDefra <62916845+JackSheppardDefra@users.noreply.github.com>
2020-06-05 15:01:25 +0100

    added tear-down bootstrap script for octopus (#48)
    Co-authored-by: Jack Sheppard <a-jack.sheppard@defracloud.onmicrosoft.com>

---
+++ bootstrap_octopus_tear-down.sh
@@ -0,0 +1,15 @@
+# !/bin/bash
+export FFFS_WEB_PORTAL_STAGING_DB_ADMIN_USERNAME='#{webPortalStagingDbAdminUsername}'
+export FFFS_WEB_PORTAL_STAGING_DB_ADMIN_PASSWORD='#{webPortalStagingDbAdminPassword}'
+export FFFS_WEB_PORTAL_STAGING_DB_CONNECTION_STRING='#{webPortalStagingDbConnectionString}'
+export FFFS_WEB_PORTAL_STAGING_DB_STAGING_SCHEMA='#{webPortalStagingDbStagingSchema}'
+export FFFS_WEB_PORTAL_STAGING_DB_USERNAME='#{webPortalStagingDbUsername}'
+export FFFS_WEB_PORTAL_STAGING_DB_REPORTING_SCHEMA='#{webPortalStagingDbReportingSchema}'
+export FFFS_WEB_PORTAL_STAGING_DB_REPORTING_USERNAME='#{webPortalStagingDbReportingUsername}'
+export FFFS_WEB_PORTAL_STAGING_DB_REPORTING_PASSWORD='#{webPortalStagingDbReportingPassword}'
+
+
+# WARNING - This command will drop the schemas defined in pom.xml.
+# Do NOT use in the production environment.
+echo "Executing Maven DB script"
+mvn liquibase:dropAll
