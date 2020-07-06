908c4e2122a8b0148c3e71df11cf66e6ac8eb7d7
pwadmore-ea <pwadmore-ea@users.noreply.github.com>
2020-07-06 13:02:29 +0100

    Remove redundant environment variables for database schema names

--- bootstrap_octopus_tear-down.sh
+++ bootstrap_octopus_tear-down.sh
@@ -2,10 +2,8 @@
 export FFFS_WEB_PORTAL_STAGING_DB_ADMIN_USERNAME='#{webPortalStagingDbAdminUsername}'
 export FFFS_WEB_PORTAL_STAGING_DB_ADMIN_PASSWORD='#{webPortalStagingDbAdminPassword}'
 export FFFS_WEB_PORTAL_STAGING_DB_CONNECTION_STRING='#{webPortalStagingDbConnectionString}'
-export FFFS_WEB_PORTAL_STAGING_DB_STAGING_SCHEMA='#{webPortalStagingDbStagingSchema}'
 export FFFS_WEB_PORTAL_STAGING_DB_USERNAME='#{webPortalStagingDbUsername}'
 export FFFS_WEB_PORTAL_STAGING_DB_PASSWORD='#{webPortalStagingDbPassword}'
-export FFFS_WEB_PORTAL_STAGING_DB_REPORTING_SCHEMA='#{webPortalStagingDbReportingSchema}'
 export FFFS_WEB_PORTAL_STAGING_DB_REPORTING_USERNAME='#{webPortalStagingDbReportingUsername}'
 export FFFS_WEB_PORTAL_STAGING_DB_REPORTING_PASSWORD='#{webPortalStagingDbReportingPassword}'
 
