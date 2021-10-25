56e3ee6246a822c1133fd29f5fb5a365da6170e6
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2021-10-25 11:23:48 +0100

    Support rollback of all Liquibase changesets (#137)
    * Support rollback of all Liquibase changesets
    
    https://eaflood.atlassian.net/browse/IWP-867
    
    This is required for disaster recovery
    
    * Add rollback script
    
    * Cosmetic change
    
    * Use Ubuntu 20.04
    
    * Update version number
    
    Co-authored-by: pwadmore-ea <pwadmore-ea@users.noreply.github.com>

---
+++ bootstrap_octopus_rollback.sh
@@ -0,0 +1,12 @@
+# !/bin/bash
+export FFFS_WEB_PORTAL_STAGING_DB_ADMIN_USERNAME='#{webPortalStagingDbAdminUsername}'
+export FFFS_WEB_PORTAL_STAGING_DB_ADMIN_PASSWORD='#{webPortalStagingDbAdminPassword}'
+export FFFS_WEB_PORTAL_STAGING_DB_CONNECTION_STRING='#{webPortalStagingDbConnectionString}'
+export FFFS_WEB_PORTAL_STAGING_DB_USERNAME='#{webPortalStagingDbUsername}'
+export FFFS_WEB_PORTAL_STAGING_DB_PASSWORD='#{webPortalStagingDbPassword}'
+export FFFS_WEB_PORTAL_STAGING_DB_REPORTING_USERNAME='#{webPortalStagingDbReportingUsername}'
+export FFFS_WEB_PORTAL_STAGING_DB_REPORTING_PASSWORD='#{webPortalStagingDbReportingPassword}'
+
+
+echo "Executing Maven DB script to rollback all Liquibase changesets."
+mvn liquibase:rollback "-Dliquibase.rollbackDate=2020-06-04"
