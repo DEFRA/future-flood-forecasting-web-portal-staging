0496de565b2adf04b682a061e641b914fa82c3c7
pwadmore-ea <pwadmore-ea@users.noreply.github.com>
2024-05-09 18:01:25 +0100

    Prevent Liquibase update before rollback/teardown

--- bootstrap_octopus_rollback.sh
+++ bootstrap_octopus_rollback.sh
@@ -9,4 +9,4 @@ export FFFS_WEB_PORTAL_STAGING_DB_REPORTING_PASSWORD='#{webPortalStagingDbReport
 
 
 echo "Executing Maven DB script to rollback all Liquibase changesets."
-mvn process-resources liquibase:rollback "-Dliquibase.rollbackDate=2020-06-04"
+mvn process-resources liquibase:rollback "-Dliquibase.rollbackDate=2020-06-04"  "-Pliquibase-without-update"
--- tear-down.sh
+++ tear-down.sh
@@ -1,4 +1,4 @@
 # !/bin/bash
 # WARNING - This command will drop the schemas defined in pom.xml.
 # Do NOT use in the production environment.
-mvn process-resources liquibase:dropAll
+mvn process-resources liquibase:dropAll  "-Pliquibase-without-update"
